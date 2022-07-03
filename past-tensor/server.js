41256d9fa35e540589c94315ba2f37323615ce67
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-03 22:27:01 +0800

    add: server

--- past-tensor/server.js
+++ past-tensor/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const init = require("./utils/init");
+const nlp = require("compromise");
+nlp.extend(require("compromise-sentences"));
 require("dotenv").config({});
 
 const app = express();
-
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", require("./routes/home.route"));
+app.get("/", (req, res) => {
+  res.render("index", { text: "" });
+});
+
+app.post("/", (req, res) => {
+  let doc = nlp(req.body.sentence);
+  doc.sentences().toPastTense();
+  const text = doc.text();
+  res.render("index", { text });
+});
 
 init(app);
