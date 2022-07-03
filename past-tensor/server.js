d8125785b003541e3bef2b0d3628ee85ab359c1f
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-03 23:03:52 +0800

    add: present text

--- past-tensor/server.js
+++ past-tensor/server.js
@@ -6,18 +6,20 @@ require("dotenv").config({});
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render("index", { text: "" });
+  res.render("index", { past: "", present: undefined });
 });
 
 app.post("/", (req, res) => {
-  let doc = nlp(req.body.sentence);
+  const present = req.body.sentence;
+  let doc = nlp(present);
   doc.sentences().toPastTense();
-  const text = doc.text();
-  res.render("index", { text });
+  const past = doc.text();
+  res.render("index", { past, present });
 });
 
 init(app);
